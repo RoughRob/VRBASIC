eb610e46d6ae64620709e7ad17bb90373c3976dc
RoughRob24 <rlrosche@gmail.com>
2017-09-01 17:13:59 -0400

    Hammer/tower builder
    added a hammer that builds the towers

---
+++ EscapeRoom-master/EscapeRoom/Assets/BuildTower.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildTower : MonoBehaviour
+{
+
+
+    public Mesh firstMesh;
+    public Mesh midMesh;
+    public Mesh finalMesh;
+
+    public GameObject Hammer;
+
+    public Vector3 hammerLocation;
+    public float hammerRotation = 60f;
+
+    public float waitTime = 5.0f;
+    public float hammerWaitTime = 8.0f;
+
+
+
+    public bool startDelay;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<MeshFilter>().sharedMesh = firstMesh;
+
+        startDelay = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<MeshFilter>().sharedMesh = midMesh;
+            startDelay = true;
+            Hammer.SetActive(true);
+
+        }
+
+        if (startDelay == true)
+        {
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0.0f)
+            {
+                GetComponent<MeshFilter>().sharedMesh = finalMesh;
+
+            }
+
+
+
+            hammerWaitTime -= Time.deltaTime;
+            if (hammerWaitTime <= 0.0f)
+            {
+                Hammer.SetActive(false);
+                startDelay = false;
+            }
+        }
+
+    }
+
+}
